7514a07adf4ddb3e2718a71c21e8dcaec1307013
Kina <35324430+Kina-Nargang@users.noreply.github.com>
2020-04-08 11:46:14 -0700

    Add project files.

---
+++ EventCatalogApi/Data/EventContext.cs
@@ -0,0 +1,142 @@
+﻿using EventCatalogApi.Domain;
+using EventCatalogApi.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogApi.Data
+{
+    public class EventContext: DbContext
+    {
+        public EventContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<EventCategory> EventCategories { get; set; }
+        public DbSet<EventType> EventTypes { get; set; }
+        public DbSet<EventDetail> EventDatails { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Table EventCategories
+            modelBuilder.Entity<EventCategory>(e =>
+            {
+                e.ToTable("EventCategories");
+
+                e.Property(g => g.Id)
+                // Gets or sets a value that instructs the serialization engine 
+                // that the member must be present when reading or deserializing.
+                 .IsRequired()
+                 .UseHiLo("event_categories_hilo");
+
+                e.Property(g => g.Category)
+                 .IsRequired()
+                 .HasMaxLength(100);
+            });
+
+            // Table EventTypes
+            modelBuilder.Entity<EventType>(e =>
+            {
+                e.ToTable("EventTypes");
+
+                e.Property(t => t.Id)
+                 .IsRequired()
+                 .UseHiLo("event_type_hilo");
+
+                e.Property(t => t.Type)
+                 .IsRequired()
+                 .HasMaxLength(100);
+            });
+
+            //Table EventDatails
+            modelBuilder.Entity<EventDetail>(e =>
+            {
+                e.ToTable("EventDetails");
+
+                e.Property(d => d.Id)
+                 .IsRequired()
+                 .UseHiLo("event_detail_hilo");
+
+                e.HasOne(d => d.EventCategory)
+                 .WithMany()
+                 .HasForeignKey(d => d.EventCategoryId);
+
+                e.HasOne(d => d.EventType)
+                 .WithMany()
+                 .HasForeignKey(d => d.EventTypeId);
+
+                e.Property(d => d.EventName)
+                 .IsRequired()
+                 .HasMaxLength(100);
+
+                e.Property(d => d.isFree)
+                 .IsRequired()
+                 .HasDefaultValue("Free")
+                 .HasMaxLength(10);
+
+                e.Property(d => d.isPublic)
+                 .IsRequired()
+                 .HasDefaultValue("Public")
+                 .HasMaxLength(10);
+
+                e.Property(d => d.PictureUrl)
+                 .HasMaxLength(200);
+
+                e.Property(d => d.Price)
+                 .IsRequired()
+                 .HasDefaultValue(0.00);
+
+                e.Property(d => d.StartDate)
+                 .IsRequired();
+
+                e.Property(d => d.EndDate)
+                 .IsRequired();
+
+                e.Property(d => d.CreatedDate)
+                .IsRequired()
+                .HasDefaultValue(DateTime.Now);
+
+                e.Property(d => d.Description)
+                 .IsRequired()
+                 .HasDefaultValue(1000);
+
+                e.Property(o => o.OrganizerName)
+                 .IsRequired()
+                 .HasMaxLength(150);
+
+                e.Property(o => o.OrganizerDescription)
+                 .IsRequired()
+                 .HasMaxLength(1000);
+
+                e.Property(l => l.LocationName)
+                  .IsRequired()
+                  .HasMaxLength(100);
+
+                e.Property(l => l.City)
+                 .IsRequired()
+                 .HasMaxLength(50);
+
+                e.Property(l => l.State)
+                 .IsRequired()
+                 .HasMaxLength(50);
+
+                e.Property(l => l.Country)
+                 .IsRequired()
+                 .HasMaxLength(100);
+
+                e.Property(l => l.ZipCode)
+                 .IsRequired()
+                 .HasMaxLength(50);
+
+                e.Property(l => l.Address)
+                 .IsRequired()
+                 .HasMaxLength(200);
+
+                e.Property(l => l.Address2)
+                 .HasMaxLength(200);                
+            });
+        }
+    }
+}
---
+++ EventCatalogApi/Migrations/20200408183253_Initial.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EventCatalogApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "event_categories_hilo",
+                incrementBy: 10);
+
+            migrationBuilder.CreateSequence(
+                name: "event_detail_hilo",
+                incrementBy: 10);
+
+            migrationBuilder.CreateSequence(
+                name: "event_type_hilo",
+                incrementBy: 10);
+
+            migrationBuilder.CreateTable(
+                name: "EventCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Category = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Type = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    EventName = table.Column<string>(maxLength: 100, nullable: false),
+                    Description = table.Column<string>(nullable: false, defaultValue: "1000"),
+                    PictureUrl = table.Column<string>(maxLength: 200, nullable: true),
+                    Price = table.Column<decimal>(nullable: false, defaultValue: 0m),
+                    isFree = table.Column<string>(maxLength: 10, nullable: false, defaultValue: "Free"),
+                    isPublic = table.Column<string>(maxLength: 10, nullable: false, defaultValue: "Public"),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false, defaultValue: new DateTime(2020, 4, 8, 11, 32, 52, 764, DateTimeKind.Local).AddTicks(7906)),
+                    OrganizerName = table.Column<string>(maxLength: 150, nullable: false),
+                    OrganizerDescription = table.Column<string>(maxLength: 1000, nullable: false),
+                    LocationName = table.Column<string>(maxLength: 100, nullable: false),
+                    Address = table.Column<string>(maxLength: 200, nullable: false),
+                    Address2 = table.Column<string>(maxLength: 200, nullable: true),
+                    City = table.Column<string>(maxLength: 50, nullable: false),
+                    State = table.Column<string>(maxLength: 50, nullable: false),
+                    ZipCode = table.Column<string>(maxLength: 50, nullable: false),
+                    Country = table.Column<string>(maxLength: 100, nullable: false),
+                    EventTypeId = table.Column<int>(nullable: false),
+                    EventCategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EventDetails_EventCategories_EventCategoryId",
+                        column: x => x.EventCategoryId,
+                        principalTable: "EventCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventDetails_EventTypes_EventTypeId",
+                        column: x => x.EventTypeId,
+                        principalTable: "EventTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventDetails_EventCategoryId",
+                table: "EventDetails",
+                column: "EventCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventDetails_EventTypeId",
+                table: "EventDetails",
+                column: "EventTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventDetails");
+
+            migrationBuilder.DropTable(
+                name: "EventCategories");
+
+            migrationBuilder.DropTable(
+                name: "EventTypes");
+
+            migrationBuilder.DropSequence(
+                name: "event_categories_hilo");
+
+            migrationBuilder.DropSequence(
+                name: "event_detail_hilo");
+
+            migrationBuilder.DropSequence(
+                name: "event_type_hilo");
+        }
+    }
+}
